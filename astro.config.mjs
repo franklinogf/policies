72bd04dfa98ecfaac0d71dc32f96c37a6a24389f
Franklin Gonzalez <franklinomarflores@gmail.com>
2025-10-02 15:01:44 -0400

    feat: add privacy policy for Domino Tracker application

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  redirects: {
+    "/": "privacy/domino-tracker",
+  },
 });
